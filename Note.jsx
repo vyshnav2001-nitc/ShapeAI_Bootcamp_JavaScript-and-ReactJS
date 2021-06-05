844e1aae16016f693a9bdcaa2ba3dbab98bebc6c
vyshnav2001-nitc <79520829+vyshnav2001-nitc@users.noreply.github.com>
2021-06-05 15:52:23 +0400

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Note(){
+  return (
+    <div className="note">
+                 <h1> Javascript and React.js</h1>
+                 <p>This was an amazing bootcamp taken by Shaurya Sinha.
+                   we covered everything from scratch including Javascript, HTML.
+                 </p>
+   </div>;
+}
+
+export default Header;
